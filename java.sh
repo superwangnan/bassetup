6d319fc89bf6c43ba81983b42cc23c916a122756
Wang, Nan <superwangnan@qq.com>
2020-10-31 10:18:50 +0800

    Create java.sh
    download sap machine and finish basic setup

---
+++ java.sh
@@ -0,0 +1,17 @@
+curl -L https://github.com/SAP/SapMachine/releases/download/sapmachine-11.0.9/sapmachine-jdk-11.0.9_linux-x64_bin.tar.gz --output java.tar.gz
+tar zvxf java.tar.gz
+mv sapmachine-jdk-11.0.9 ~/.sapmachine-jdk-11.0.9
+echo export PATH=$PATH:/home/user/.sapmachine-jdk-11.0.9/bin >> ~/.bashrc
+echo export JAVA_HOME=/home/user/.sapmachine-jdk-11.0.9 >> ~/.bashrc
+rm -f java.tar.gz
+
+curl -L https://downloads.apache.org/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz --output mvn.tar.gz
+tar zvxf mvn.tar.gz
+mv apache-maven-3.6.3 ~/.apache-maven-3.6.3
+echo MVN_HOME=~/.apache-maven-3.6.3 >> ~/.bashrc
+echo PATH=$PATH:$MVN_HOME/bin >> ~/.bashrc
+rm -f mvn.tar.gz
+
+curl https://mirrors.huaweicloud.com/mirror/v1/configurations/maven? --output ~/.m2/settings.xml
+
+
